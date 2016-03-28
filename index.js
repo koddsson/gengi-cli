08e17636be12be40641e945b0b1e9d2ceecf6245
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-03-28 03:00:35 +0000

    Implement .eslintrc with airbnb/base

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const process = require('process');
-const fetch = require('node-fetch');
+const process = require('process')
+const fetch = require('node-fetch')
 
-const args = process.argv.slice(2);
-const currency = args[0];
-const value = args[1];
+const args = process.argv.slice(2)
+const currency = args[0]
+const value = args[1]
 
 fetch(`http://api.gengi.is/calculate/${currency}/${value}`).then(response => {
   if (response.status !== 200) {
-    var error = new Error(response.statusText)
+    const error = new Error(response.statusText)
     error.response = response
     throw error
   } else {
-    return response;
+    return response
   }
 }).then(
   res => res.json()
 ).then(json => {
-  const value = `${currency}value`;
-  if (value in json) {
-    console.log(json[value]);
+  const propName = `${currency}value`
+  if (propName in json) {
+    console.log(json[propName])
   } else {
-    console.log(json);
+    console.log(json)
   }
-}).catch(err => console.log(err.message));
+}).catch(err => console.log(err.message))
