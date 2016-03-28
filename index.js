6f4ca4f5eb4209b23bcfbcfd956467d5e378a9d8
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-03-28 03:47:45 +0000

    Break stuff up, create instructions
    Show usage instructions if currency code is invalid

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-const fetch = require('node-fetch')
+const gengi = require('./lib/gengi')
+const help = require('./lib/help')
 
 const args = process.argv.slice(2)
 const currency = args[0]
 const value = parseFloat(args[1]) || 1
 
-if (!currency) {
-  // TODO: Do this more elegantly! F.x. show usage instructions
-  const error = new Error('No currency provided!')
-  throw error
+if (currency && currency.length === 3) {
+  gengi(currency, value)
+} else {
+  help()
 }
-
-fetch(`http://api.gengi.is/calculate/${currency}/${value}`).then(response => {
-  if (response.status !== 200) {
-    const error = new Error(response.statusText)
-    error.response = response
-    throw error
-  } else {
-    return response
-  }
-}).then(
-  res => res.json()
-).then(json => {
-  const propName = `${currency.toUpperCase()}value`
-  if (propName in json) {
-    console.log(json[propName])
-  } else {
-    console.log(json)
-  }
-}).catch(err => console.log(err.message))
