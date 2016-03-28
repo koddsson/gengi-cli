481f1e5e5b294fc054ae2cdf3d54262ae5faf3ca
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-03-28 22:13:08 +0000

    Implement babeljs

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const gengi = require('./lib/gengi')
-const help = require('./lib/help')
-
-const args = process.argv.slice(2)
-const currency = args[0]
-const value = parseFloat(args[1]) || 1
-
-if (currency && currency.length === 3) {
-  gengi(currency, value)
-} else {
-  help()
-}
+require('babel-register')()
+require('./lib/index')
